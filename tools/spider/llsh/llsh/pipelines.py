6e05e44d77e67bd5bf344d250af21515fecb2241
Chenyangsut <sygysut@163.com>
2021-01-03 17:25:09 +0800

    change llsh pipelines, add read history date

--- tools/spider/llsh/llsh/pipelines.py
+++ tools/spider/llsh/llsh/pipelines.py
@@ -8,13 +8,26 @@
 import time
 import shutil
 
-
 class LlshPipeline:
+    __slots__ = ["data", "file"]
+
     def __init__(self):
-        shutil.copy("all_magnetic.txt", "spider_file/llsh/{}.txt".format(time.strftime("%Y-%m-%d_%H_%M_%S")))
-        self.file = open("all_magnetic.txt", "w", encoding="gb18030")  # 保存文件
+        self.data = self.read_data("E:/cy/data/spider_file/llsh/all_magnetic.txt")
+        shutil.copy("E:/cy/data/spider_file/llsh/all_magnetic.txt", "E:/cy/data/spider_file/llsh/{}.txt".format(time.strftime("%Y-%m-%d_%H_%M_%S")))
+        self.file = open("E:/cy/data/spider_file/llsh/all_magnetic.txt", "w", encoding="gb18030")  # 保存文件
         self.file.write("******\ndownload_time:{}\n******\n".format(time.strftime("%Y-%m-%d %H:%M:%S")))  # 记录下载时间
 
+    def read_data(self, original_file):
+        original_data = open(original_file, "r", encoding="gb18030")
+        try:
+            data_list = [line.split("name:")[-1].strip() for line in original_data if line.startswith("name:")]
+        except:
+            data_list = []
+        finally:
+            original_data.close()
+        return set(data_list)
+
     def process_item(self, item, spider):
-        self.file.write("name:{}\nurl :{}\nlink:{}\n******\n".format(item["name"], item["url"], item["magnetic"]))  # 写数据
+        if item["name"].strip() not in self.data:
+            self.file.write("name:{}\nurl :{}\nlink:{}\n******\n".format(item["name"], item["url"], item["magnetic"]))  # 写数据
         return item
