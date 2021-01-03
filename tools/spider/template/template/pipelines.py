d8c22a6c30ca980387e8b71d3014cba2eb78cc7b
Chenyangsut <sygysut@163.com>
2021-01-03 17:37:17 +0800

    add prepare and some item

--- tools/spider/template/template/pipelines.py
+++ tools/spider/template/template/pipelines.py
@@ -8,9 +8,26 @@
 
 class TemplatePipeline:
     def __init__(self):
+        pass
+
+    def open_spider(self, spider):
         self.file = open("download_magnet.txt", "w", encoding="gb18030")
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    # # (可选)此方法如果实现了,那么BookFilterPipeline对象从这里调用,必须返回一个cls(参数)对象,crawler.settings是读取项目目录下的settings中的配置选项！
+    # @classmethod
+    # def from_crawler(cls, crawler):
+    #     count = crawler.settings.get('BOOK_FILTER_COUNT', 0)
+    #     return cls(count)
 
     def process_item(self, item, spider):
         # 将下载后的链接储存到临时文件中
         self.file.write("{}\n".format(item["magnet"]))
 
+
+class PrepareItemPipeline:
+
+    def process_item(self, item, spider):
+        pass
